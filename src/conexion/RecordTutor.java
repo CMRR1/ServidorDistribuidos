10bc0ea5c9b2deae4f78afbf3dd48afd05648e14
Aranda037 <50560261+Aranda037@users.noreply.github.com>
2020-05-19 14:27:59 -0700

    Web Socket
    Ya tiene web socket y llama a la bd para sacar a los tutores.

---
+++ src/conexion/RecordTutor.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import negociodistribuidos.Alumno;
+import negociodistribuidos.Asignacion;
+import negociodistribuidos.Tutor;
+
+/**
+ *
+ * @author arand
+ */
+public class RecordTutor {
+    
+    public ArrayList<Tutor> getTutores(Connection con){
+        String sql = "SELECT * FROM tutores";
+        ResultSet rs;
+        CallableStatement cst;
+        ArrayList<Tutor> tutores = new ArrayList<Tutor>();
+        try {
+            cst = con.prepareCall(sql);
+            rs = cst.executeQuery();
+            while(rs.next()){
+                Tutor tut= new Tutor(
+                    rs.getString("nombre"),
+                    rs.getString("usuario"),
+                    rs.getString("pass"),
+                    rs.getInt("id"),
+                    rs.getInt("id_alumno")
+                );
+                
+                tutores.add(tut);
+
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(RecordTutor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        return tutores;
+    } 
+    
+}
--- src/servidordistribuidos/ServidorDistribuidos.java
+++ src/servidordistribuidos/ServidorDistribuidos.java
@@ -5,17 +5,81 @@
  */
 package servidordistribuidos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import conexion.RecordTutor;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import negociodistribuidos.Tutor;
+
 /**
  *
  * @author ceccy
  */
 public class ServidorDistribuidos {
 
+    public static final String URL = "jdbc:mysql://localhost:3306/distribuidos?useTimezone=true&serverTimezone=UTC";
+    public static final String user = "root";
+    public static final String pass = "root";
+
+    //Sentencia que se le envía al manejador de la base de datos para ejecutar un SP
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+
+            ServerSocket server = null;
+            Socket sc = null;
+            int puerto = 5000;
+            server = new ServerSocket(puerto);
+            DataInputStream in;
+            DataOutputStream out;
+            ObjectOutputStream objOut;
+            ArrayList<Tutor> tutores = new ArrayList<Tutor>();
+
+            while (true) {
+                sc = server.accept();
+                System.out.println("INICIO");
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+                objOut = new ObjectOutputStream(sc.getOutputStream());
+
+                try {
+                    // TODO code application logic here
+                    //private static final String url ="jdbc:mysql://192.168.1.90:3306/fhdb?useTimezone=true&serverTimezone=UTC";
+                    Connection con = DriverManager.getConnection(URL, user, pass);
+
+                    RecordTutor re = new RecordTutor();
+                    System.out.println(re.getTutores(con));
+                    tutores = re.getTutores(con);
+                    String mensaje = in.readUTF();
+                    System.out.println(mensaje);
+                    out.writeInt(tutores.size());
+                    for (Tutor tutore : tutores) {
+                        System.out.println(tutore);
+                        objOut.writeObject(tutore);
+                    }
+                    System.out.println("Lista enviada");
+                    sc.close();
+                    System.out.println("FIN");
+                    con.close();
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
-    
+
 }
